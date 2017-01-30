04c8ac14cad3b01248e9039e39a660c7f2e88190
emmawh30 <emma.white30@gmail.com>
2017-01-30 09:09:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var todos = ["Shopping List"];
+
+var input = prompt("What Would You Like To Do?");
+
+
+while(input !== "quit"){
+
+//handle input
+	if(input === "list") {
+		console.log("**********")
+		todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+		
+	});
+		console.log("**********")
+
+	} 	else if(input === "new") {
+		//ask for new todo
+		var newTodo = prompt("Enter new todo");
+		//add to todos array
+		todos.push(newTodo);
+	
+
+	}	else if(input === "delete"){
+		//ask for index of todo to be deleted
+		var index = prompt("Enter index of todo to delete"); 
+		//delete that todo
+		//splice () which is how many items after the index do we want to delete?
+		todos.splice(index,1);
+	}
+		//ask again for new input
+		input = prompt("What Would You Like To Do?")
+	}
+		console.log("ok, you quit the app");
